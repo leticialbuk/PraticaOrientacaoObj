5be140e5a4be44eb16ab215c5b07115fe476adc3
leticialbuk <leticia.albuk@gmail.com>
2023-04-24 23:58:16 -0300

    Ordenando listas

---
+++ Content.cs
@@ -0,0 +1,18 @@
+namespace Balta.ContentContext
+{
+    public abstract class Content
+    {
+        public Content(string title, string url)
+        {
+            Title = title;
+            Url = url;
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Balta.ContentContext;
+using Balta.ContentContext.Enums;
+using System.Linq;
+
+namespace PraticaOrientacaoObj
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var articles = new List<Article>();
+            articles.Add(new Article("Artigo sobre OOP", "orientacao-objetos"));
+            articles.Add(new Article("Artigo sobre C#", "csharp"));
+            articles.Add(new Article("Artigo sobre .Net", "dotnet"));
+
+            foreach (var article in articles)
+            {
+                Console.WriteLine(article.Id);
+                Console.WriteLine(article.Title);
+                Console.WriteLine(article.Url);
+            }
+
+            var courses = new List<Course>();
+            var courseOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+            var courseCsharp = new Course("Fundamentos C#", "fundamentos-csharp");
+            var courseAspNet = new Course("Fundamentos ASP.NET", "fundamentos-aspnet");
+            courses.Add(courseOOP);
+            courses.Add(courseCsharp);
+            courses.Add(courseAspNet);
+
+            var careers = new List<Career>();
+            var careerDotnet = new Career("Especialista .Net", "especialista-dotnet");
+            var careerItem = new CareerItem(2, "OOP", "", null);
+            var careerItem2 = new CareerItem(1, "Comece por aqui", "", null);
+            var careerItem3 = new CareerItem(3, "Aprenda .NET", "", null);
+            careerDotnet.Items.Add(careerItem2);
+            careerDotnet.Items.Add(careerItem);
+            careerDotnet.Items.Add(careerItem3);
+            careers.Add(careerDotnet);
+
+            foreach (var career in careers)
+            {
+                Console.WriteLine(career.Title);
+                foreach (var item in career.Items.OrderBy(x => x.Order))
+                {
+                    Console.WriteLine($"{item.Order} - {item.Title}");
+                }
+            }
+        }
+    }
+}
